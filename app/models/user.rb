6fb0133f08bb2286a4a40fc07058b687643d4a1b
EC2 Default User <ec2-user@ip-172-31-27-149.us-east-2.compute.internal>
2021-03-01 08:36:28 +0000

    kadaidayo2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord  
     validates :name, presence: true, length: { maximum: 15 }
-    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-    validates :password, :password_confirmation, predence: true, 
-    format: { with: /^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}$/i }
+    validates :email, presence: true, format: { with: /[a-zA-Z0-9._+]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*/ }
+    validates :password, :password_confirmation, presence: true, 
+    format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i }
     has_secure_password
 end
 
